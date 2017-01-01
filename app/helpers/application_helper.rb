faa0761da5d591bbe04478f8a4a0a45fbd6c7242
oliver chen <olichen@ucdavis.edu>
2016-12-31 19:23:18 -0500

    added dynamic page stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def page_title(title = '')
+    base_title = 'Pokemon Data Analysis'
+    if title.empty?
+      return base_title
+    else
+      return title + ' | ' + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Pokemon Data Analysis"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Pokemon Data Analysis"
+  end
 end
